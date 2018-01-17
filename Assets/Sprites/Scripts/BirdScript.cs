ee8493a842e05eb696066cb5924cb41d683be058
ElRMapache <alo15470@uvg.edu.gt>
2018-01-17 10:24:15 -0600

    escena 1

--- Assets/Sprites/Scripts/BirdScript.cs
+++ Assets/Sprites/Scripts/BirdScript.cs
@@ -3,17 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BirdScript : MonoBehaviour {
-
+    float jumpForce = 300f;
+    float forwardSpeed = 2f;
+    public bool dead = false; 
+    public GameObject cam;
+    public Rigidbody2D pajaro;
+    
 	// Use this for initialization
 	void Start () {
-        Debug.Log("Start");
+        pajaro = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetButtonDown("Jump"))
+        //   pajaro.velocity = Vector2.zero;
+        if (dead == false)
         {
-            Debug.Log("Espacio");
+            if (Input.GetButtonDown("Jump"))
+            {
+                pajaro.AddForce(Vector2.up * jumpForce);
+            }
+            pajaro.transform.Translate(new Vector3(1, 0, 0) * forwardSpeed * Time.deltaTime);
+            cam.transform.Translate(new Vector3(1, 0, 0) * forwardSpeed * Time.deltaTime);
+           // if (pajaro.position.Translate(16, 0, 0)) 
+           // {
+           //     dead = true;
+           // }
         }
+
 	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        dead = true;
+    }
+
 }
